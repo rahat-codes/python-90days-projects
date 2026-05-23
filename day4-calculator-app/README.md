718bfa3f3f4e36ba1c7d70672c6409858b40e9b8
Rahat <rahatmdmahadihasan@gmail.com>
2026-05-24 00:48:58 +0900

    Day 4 calculator app completed

---
+++ day4-calculator-app/README.md
@@ -0,0 +1,17 @@
+# Day 4 - Calculator App
+
+## Features
+
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Error handling
+
+## Concepts Learned
+
+- Functions
+- Loops
+- User input
+- Error handling
+- Conditional statements
---
+++ day4-calculator-app/calculator.py
@@ -0,0 +1,49 @@
+def add(a,b):
+    return a + b
+
+def substract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    if b == 0:
+        print("Error! Cannot devide by zero.")
+    return a / b
+
+while True:
+    print("\n===== PYTHON CALCULATOR =====")
+    print("1. Add")
+    print("2. Substract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = int(input("Choose operation (1-5): "))
+
+    if choice == 5:
+        print("Calculator is closed.")
+        break
+
+    if choice not in [1,2,3,4]:
+        print("Invalied choice. Try again.")
+        continue
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == 1:
+            print("Result: ", add(num1, num2))
+
+        elif choice == 2:
+            print("Result: ", substract(num1, num2))
+        
+        elif choice == 3:
+            print("Result: ", multiply(num1, num2))
+
+        else:
+            print("Result: ", divide(num1, num2))
+    except ValueError:
+        print("Invalid input! Please enter numbers only.")
